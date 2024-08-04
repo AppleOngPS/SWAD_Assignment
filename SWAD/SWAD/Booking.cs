84ccb6ac5def28870f72a6b747f8d3b1fe7b4c92
AppleOngPS <148023920+AppleOngPS@users.noreply.github.com>
2024-08-05 01:36:10 +0800

    reserve car showing all date time

--- SWAD/SWAD/Booking.cs
+++ SWAD/SWAD/Booking.cs
@@ -6,7 +6,56 @@ using System.Threading.Tasks;
 
 namespace SWAD
 {
-    internal class Booking
+    public class Booking
     {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private TimeOnly startTime;
+        public TimeOnly StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+        private TimeOnly endTime;
+        public TimeOnly EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+        private DateOnly startDate;
+        public DateOnly StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+        private DateOnly endDate;
+        public DateOnly EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public Booking() { }
+
+        public Booking(int i , DateOnly sd , TimeOnly st, DateOnly ed, TimeOnly et)
+        {
+            id = i;
+            startDate = sd;
+            startTime = st;
+            endDate = sd;
+            endTime = st;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Booking ID: {Id}\nStart Date: {StartDate}\nStart Time: {StartTime}\nEnd Date: {EndDate}\nEnd Time: {EndTime}";
+        }
+
     }
 }
--- SWAD/SWAD/Program.cs
+++ SWAD/SWAD/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using SWAD;
+
 void DisplayMenu()
 {
     Console.WriteLine("Enter Your Option: ");
@@ -8,7 +10,7 @@ void DisplayMenu()
     Console.WriteLine("[0] Exit");
     Console.WriteLine("Enter Your option: ");
 }
-
+List<Booking> sList = new List<Booking>();
 int option;
 while (true)
 {
@@ -16,7 +18,17 @@ while (true)
     option = Convert.ToInt32(Console.ReadLine());
     if (option == 1)
     {
-        Console.WriteLine();
+        InitTimeList(sList);
+        Console.WriteLine("Bookings Initialized.");
+        foreach (var booking in sList)
+        {
+            Console.WriteLine($"Booking ID: {booking.Id}");
+            Console.WriteLine($"Start Date: {booking.StartDate}");
+            Console.WriteLine($"Start Time: {booking.StartTime}");
+            Console.WriteLine($"End Date: {booking.EndDate}");
+            Console.WriteLine($"End Time: {booking.EndTime}");
+            Console.WriteLine(); // For better readability
+        }
     }
 
     else if (option == 2)
@@ -39,4 +51,27 @@ while (true)
         Console.WriteLine("You have enter invalid option!");
 
     }
+}
+
+static void InitTimeList(List<Booking> sList)
+{
+    // Sample date and time for initialization
+    DateOnly date1 = new DateOnly(2024, 8, 5);
+    TimeOnly startTime1 = new TimeOnly(10, 0);
+    DateOnly date2 = new DateOnly(2024, 8, 6);
+    TimeOnly endTime1 = new TimeOnly(11, 0);
+
+    Booking time1 = new Booking(1, date1, startTime1, date2, endTime1);
+    Booking time2 = new Booking(2, date1, startTime1, date2, endTime1);
+    Booking time3 = new Booking(3, date1, startTime1, date2, endTime1);
+    Booking time4 = new Booking(4, date1, startTime1, date2, endTime1);
+    Booking time5 = new Booking(5, date1, startTime1, date2, endTime1);
+    Booking time6 = new Booking(6, date1, startTime1, date2, endTime1);
+
+    sList.Add(time1);
+    sList.Add(time2);
+    sList.Add(time3);
+    sList.Add(time4);
+    sList.Add(time5);
+    sList.Add(time6);
 }
