07dfc0dca6d32c4b6305825f4388b0efba4a3c99
AppleOngPS <148023920+AppleOngPS@users.noreply.github.com>
2024-08-05 00:01:02 +0800

    Created menu

---
+++ SWAD/SWAD/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+void DisplayMenu()
+{
+    Console.WriteLine("[1] Reserve Car");
+    Console.WriteLine("[2] option 2");
+    Console.WriteLine("[3] option 3");
+}
+
+int option;
+while (true)
+{
+    DisplayMenu();
+    option = Convert.ToInt32(Console.ReadLine());
+    if (option == 1)
+    {
+        Console.WriteLine();
+    }
+
+    else if (option == 2)
+    {
+        Console.WriteLine();
+    }
+    else if (option == 3)
+    {
+        Console.WriteLine();
+    }
+    else
+    {
+        Console.WriteLine("You have exit the program!");
+        break;
+    }
+}
+
+
