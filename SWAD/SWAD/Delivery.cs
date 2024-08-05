38d7c8a8ff8deeba03104f51ec662f04c6c472b9
AppleOngPS <148023920+AppleOngPS@users.noreply.github.com>
2024-08-06 02:01:54 +0800

    completed delivery class

--- SWAD/SWAD/Delivery.cs
+++ SWAD/SWAD/Delivery.cs
@@ -8,5 +8,44 @@ namespace SWAD
 {
     internal class Delivery
     {
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private int amount;
+        public int Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+        private string location;
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+     
+
+
+
+
+        public Delivery(int i , int a , string l)
+        {
+            id = i;
+            amount = a;
+            location = l;
+
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"id: {id}\namount: {amount}\nlocation: {location}";
+        }
     }
+}
 }
